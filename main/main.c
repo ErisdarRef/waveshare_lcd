ce282fb936efc15620d1c00be01b5cb7584a16e3
Justin Reina <justinmreina@gmail.com>
2025-06-05 18:22:07 -0500

    (C)    update demo to use explicit demo selection

--- main/main.c
+++ main/main.c
@@ -19,6 +19,25 @@
 
 
 //************************************************************************************************//
+//                                        DEFINITIONS & TYPES                                     //
+//************************************************************************************************//
+
+//-----------------------------------------  Definitions -----------------------------------------//
+
+//Hardware Config
+#define TURN_LCD_ON                                 /* how waveshare demo was published, pick one */
+//#define TURN_LCD_OFF
+
+
+//Demo Select
+//#define USE_STRESS_DEMO                           /* how waveshare demo was published, pick one */
+//#define USE_BENCHMARK_DEMO
+#define USE_MUSIC_DEMO
+//#define USE_WIDGETS_DEMO
+//#define USE_UI_DEMO
+
+
+//************************************************************************************************//
 //                                          PUBLIC FUNCTIONS                                      //
 //************************************************************************************************//
 
@@ -36,26 +55,46 @@
  *
  *  @pre    second stage bootloader
  *  @post   no return
+ *
+ *  @section    Opens
+ *      pull port lock out of nested conditional statement
  */
 /**************************************************************************************************/
 void app_main(void) {
 
+    //Init
     waveshare_esp32_s3_rgb_lcd_init();              /* Initialize the Waveshare ESP32-S3 RGB LCD  */
-    waveshare_rgb_lcd_bl_on();                      /* Turn on the screen backlight               */
-    waveshare_rgb_lcd_bl_off();                     /* Turn off the screen backlight              */
 
-    
+    //Setup
+#ifdef TURN_LCD_ON
+       waveshare_rgb_lcd_bl_on();                   /* Turn on the screen backlight               */
+#else
+       waveshare_rgb_lcd_bl_off();                  /* Turn off the screen backlight              */
+#endif
+     
+    //Notify
     ESP_LOGI(TAG, "Display LVGL demos");
     
     // Lock the mutex due to the LVGL APIs are not thread-safe
     if (lvgl_port_lock(-1)) {
-        
-        // lv_demo_stress();
-        // lv_demo_benchmark();
-        // lv_demo_music();
-        // lv_demo_widgets();
+
+#ifdef USE_STRESS_DEMO
+        lv_demo_stress();
+
+#elif defined(USE_BENCHMARK_DEMO)
+        lv_demo_benchmark();
+
+#elif defined(USE_MUSIC_DEMO)
+        lv_demo_music();
+
+#elif defined(USE_WIDGETS_DEMO)
+        lv_demo_widgets();
+
+#elif defined(USE_UI_DEMO)
         example_lvgl_demo_ui();
-        
+
+#endif
+
         // Release the mutex
         lvgl_port_unlock();
     }
